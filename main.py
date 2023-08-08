f41362cdc3544f99c83d41af1bede45209f30066
TasnimJahinMowla <mahiyatasnim200021@gmail.com>
2023-08-09 00:16:59 +0600

    Function

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ print("Hello World!")
 
 for i in range(1,10):
     print(i)
+
+def evenOdd(n):
+    if(n%2==0):
+        print("Even")
+    else:
+        print("Odd")
+
+
+n=int(input("Enter an integer: "))
+evenOdd(n)
